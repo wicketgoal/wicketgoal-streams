a8a54f23047b28ddfc3d69dc9841fc988e05300d
wicketgoal <148673605+wicketgoal@users.noreply.github.com>
2025-10-10 22:26:52 +0545

    Update streams.js

--- streams.js
+++ streams.js
@@ -9,7 +9,7 @@ const streamMap = {
     poster: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEicrdB4KjEFlxTas4CXfmJLSzF3FmLOJx7YO3xsSLBykJ_a4Ot2sbG8Ou80i4aXA-x04AmitkFBEsrOdJzrFOyGhc4u-RgmdwAhOE1V81WxD4WG3CR2YGuXrBvUap6haFQ5gmvI5ti_vuT7TMqzzrN5yjOS6y2wlaIESvcANC4qxCd2H8-l57HXi4t3fUQ/s320/Copy%20of%20Copy%20of%20Nepal%20vs%20Qatar%20(3).png"
   },
   fancode: {
-    url: "https://in-mc-fdlive.fancode.com/mumbai/137180_english_hls_bebc7389b234502ta-di_h264/index.m3u8",
+    url: "https://hugh.cdn.rumble.cloud/live/t34ch9px/slot-79/yhjd-fwvx/chunklist.m3u8",
     poster: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEicrdB4KjEFlxTas4CXfmJLSzF3FmLOJx7YO3xsSLBykJ_a4Ot2sbG8Ou80i4aXA-x04AmitkFBEsrOdJzrFOyGhc4u-RgmdwAhOE1V81WxD4WG3CR2YGuXrBvUap6haFQ5gmvI5ti_vuT7TMqzzrN5yjOS6y2wlaIESvcANC4qxCd2H8-l57HXi4t3fUQ/s320/Copy%20of%20Copy%20of%20Nepal%20vs%20Qatar%20(3).png"
   },
   sky: {
