4f18c8e2da31a41a91674ca49adaaa95b6b368b8
wicketgoal <148673605+wicketgoal@users.noreply.github.com>
2025-10-10 12:03:43 +0545

    Create streams.js

---
+++ streams.js
@@ -0,0 +1,19 @@
+// streams.js
+const streamMap = {
+  nepal: {
+    url: "https://example.com/nepal.m3u8",
+    poster: "https://i.imgur.com/example.jpg"
+  },
+  vietnam: {
+    url: "https://example.com/vietnam.m3u8",
+    poster: "https://i.imgur.com/example2.jpg"
+  },
+  kuwait: {
+    url: "https://example.com/kuwait.m3u8",
+    poster: "https://i.imgur.com/example3.jpg"
+  },
+  sky: {
+    url: "https://example.com/sky.m3u8",
+    poster: "https://i.imgur.com/example4.jpg"
+  }
+};
