fedf18e3c54d09f85908ab79803442938f25b6f2
wicketgoal <148673605+wicketgoal@users.noreply.github.com>
2025-10-10 12:17:29 +0545

    Update streams.js

--- streams.js
+++ streams.js
@@ -1,19 +1,19 @@
 // streams.js
 const streamMap = {
   nepal: {
-    url: "https://example.com/nepal.m3u8",
-    poster: "https://i.imgur.com/example.jpg"
+    url: "https://hugh.cdn.rumble.cloud/live/1n1ugl24/slot-27/0dgx-829s/chunklist.m3u8",
+    poster: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEicrdB4KjEFlxTas4CXfmJLSzF3FmLOJx7YO3xsSLBykJ_a4Ot2sbG8Ou80i4aXA-x04AmitkFBEsrOdJzrFOyGhc4u-RgmdwAhOE1V81WxD4WG3CR2YGuXrBvUap6haFQ5gmvI5ti_vuT7TMqzzrN5yjOS6y2wlaIESvcANC4qxCd2H8-l57HXi4t3fUQ/s320/Copy%20of%20Copy%20of%20Nepal%20vs%20Qatar%20(3).png"
   },
   vietnam: {
     url: "https://example.com/vietnam.m3u8",
-    poster: "https://i.imgur.com/example2.jpg"
+    poster: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEicrdB4KjEFlxTas4CXfmJLSzF3FmLOJx7YO3xsSLBykJ_a4Ot2sbG8Ou80i4aXA-x04AmitkFBEsrOdJzrFOyGhc4u-RgmdwAhOE1V81WxD4WG3CR2YGuXrBvUap6haFQ5gmvI5ti_vuT7TMqzzrN5yjOS6y2wlaIESvcANC4qxCd2H8-l57HXi4t3fUQ/s320/Copy%20of%20Copy%20of%20Nepal%20vs%20Qatar%20(3).png"
   },
   kuwait: {
     url: "https://example.com/kuwait.m3u8",
-    poster: "https://i.imgur.com/example3.jpg"
+    poster: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEicrdB4KjEFlxTas4CXfmJLSzF3FmLOJx7YO3xsSLBykJ_a4Ot2sbG8Ou80i4aXA-x04AmitkFBEsrOdJzrFOyGhc4u-RgmdwAhOE1V81WxD4WG3CR2YGuXrBvUap6haFQ5gmvI5ti_vuT7TMqzzrN5yjOS6y2wlaIESvcANC4qxCd2H8-l57HXi4t3fUQ/s320/Copy%20of%20Copy%20of%20Nepal%20vs%20Qatar%20(3).png"
   },
   sky: {
     url: "https://example.com/sky.m3u8",
-    poster: "https://i.imgur.com/example4.jpg"
+    poster: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEicrdB4KjEFlxTas4CXfmJLSzF3FmLOJx7YO3xsSLBykJ_a4Ot2sbG8Ou80i4aXA-x04AmitkFBEsrOdJzrFOyGhc4u-RgmdwAhOE1V81WxD4WG3CR2YGuXrBvUap6haFQ5gmvI5ti_vuT7TMqzzrN5yjOS6y2wlaIESvcANC4qxCd2H8-l57HXi4t3fUQ/s320/Copy%20of%20Copy%20of%20Nepal%20vs%20Qatar%20(3).png"
   }
 };
