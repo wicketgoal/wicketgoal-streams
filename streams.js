9b78ec0265d5b50c582b5049662e6485ef22c744
wicketgoal <148673605+wicketgoal@users.noreply.github.com>
2025-10-15 12:53:28 +0545

    Update streams.js

--- streams.js
+++ streams.js
@@ -2,18 +2,18 @@
 const streamMap = {
   nepal: {
     url: "https://rumble-foxo.cdn.rumble.cloud/live/t5qc33px/slot-131/k3xj-agbx_1080p/playlist.m3u8",
-    poster: "https://nirutv.online/logo/image.png"
+    poster: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiw5AUFwWq2uRT3Zoqjb9XCZHLKXN9C3XsmZYRZ1IPPP_EGSvchyoTf32GNS4XuBhOXJqTy8rUflS2A4VW-0i8f-aQkza4CJkF8ifHPSRJXNTp6nGsXSg-A36ZDWKpykSz2um5oAc8JGFOCZQ-aptX1wBGZCgjF5pkyQNOISu2HQ4_jw1G7YeqKLTDbMNo/s16000/Untitled%20design%20(6).png"
   },
   ktv: {
     url: "https://webott.viatv.com.np/v0t1/KntCineplexTv.stream/playlist.m3u8",
-    poster: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEicrdB4KjEFlxTas4CXfmJLSzF3FmLOJx7YO3xsSLBykJ_a4Ot2sbG8Ou80i4aXA-x04AmitkFBEsrOdJzrFOyGhc4u-RgmdwAhOE1V81WxD4WG3CR2YGuXrBvUap6haFQ5gmvI5ti_vuT7TMqzzrN5yjOS6y2wlaIESvcANC4qxCd2H8-l57HXi4t3fUQ/s320/Copy%20of%20Copy%20of%20Nepal%20vs%20Qatar%20(3).png"
+    poster: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiw5AUFwWq2uRT3Zoqjb9XCZHLKXN9C3XsmZYRZ1IPPP_EGSvchyoTf32GNS4XuBhOXJqTy8rUflS2A4VW-0i8f-aQkza4CJkF8ifHPSRJXNTp6nGsXSg-A36ZDWKpykSz2um5oAc8JGFOCZQ-aptX1wBGZCgjF5pkyQNOISu2HQ4_jw1G7YeqKLTDbMNo/s16000/Untitled%20design%20(6).png"
   },
   fancode: {
     url: "https://kasin-tv.com/hi/play.php?id=137184",
-    poster: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEicrdB4KjEFlxTas4CXfmJLSzF3FmLOJx7YO3xsSLBykJ_a4Ot2sbG8Ou80i4aXA-x04AmitkFBEsrOdJzrFOyGhc4u-RgmdwAhOE1V81WxD4WG3CR2YGuXrBvUap6haFQ5gmvI5ti_vuT7TMqzzrN5yjOS6y2wlaIESvcANC4qxCd2H8-l57HXi4t3fUQ/s320/Copy%20of%20Copy%20of%20Nepal%20vs%20Qatar%20(3).png"
+    poster: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiw5AUFwWq2uRT3Zoqjb9XCZHLKXN9C3XsmZYRZ1IPPP_EGSvchyoTf32GNS4XuBhOXJqTy8rUflS2A4VW-0i8f-aQkza4CJkF8ifHPSRJXNTp6nGsXSg-A36ZDWKpykSz2um5oAc8JGFOCZQ-aptX1wBGZCgjF5pkyQNOISu2HQ4_jw1G7YeqKLTDbMNo/s16000/Untitled%20design%20(6).png"
   },
   sky: {
     url: "https://rumble-foxo.cdn.rumble.cloud/live/k5e12sb4/slot-5/8vx9-w6d8/playlist.m3u8",
-    poster: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEicrdB4KjEFlxTas4CXfmJLSzF3FmLOJx7YO3xsSLBykJ_a4Ot2sbG8Ou80i4aXA-x04AmitkFBEsrOdJzrFOyGhc4u-RgmdwAhOE1V81WxD4WG3CR2YGuXrBvUap6haFQ5gmvI5ti_vuT7TMqzzrN5yjOS6y2wlaIESvcANC4qxCd2H8-l57HXi4t3fUQ/s320/Copy%20of%20Copy%20of%20Nepal%20vs%20Qatar%20(3).png"
+    poster: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiw5AUFwWq2uRT3Zoqjb9XCZHLKXN9C3XsmZYRZ1IPPP_EGSvchyoTf32GNS4XuBhOXJqTy8rUflS2A4VW-0i8f-aQkza4CJkF8ifHPSRJXNTp6nGsXSg-A36ZDWKpykSz2um5oAc8JGFOCZQ-aptX1wBGZCgjF5pkyQNOISu2HQ4_jw1G7YeqKLTDbMNo/s16000/Untitled%20design%20(6).png"
   }
 };
