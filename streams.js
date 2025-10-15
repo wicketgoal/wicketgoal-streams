597662f6a03953999520add1cbf058f7ef8dec3e
wicketgoal <148673605+wicketgoal@users.noreply.github.com>
2025-10-15 12:36:40 +0545

    Update streams.js

--- streams.js
+++ streams.js
@@ -1,8 +1,8 @@
 // streams.js
 const streamMap = {
   nepal: {
-    url: "https://hugh.cdn.rumble.cloud/live/gi29le7p/slot-15/1i5g-ferp/chunklist.m3u8",
-    poster: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEicrdB4KjEFlxTas4CXfmJLSzF3FmLOJx7YO3xsSLBykJ_a4Ot2sbG8Ou80i4aXA-x04AmitkFBEsrOdJzrFOyGhc4u-RgmdwAhOE1V81WxD4WG3CR2YGuXrBvUap6haFQ5gmvI5ti_vuT7TMqzzrN5yjOS6y2wlaIESvcANC4qxCd2H8-l57HXi4t3fUQ/s320/Copy%20of%20Copy%20of%20Nepal%20vs%20Qatar%20(3).png"
+    url: "https://rumble-foxo.cdn.rumble.cloud/live/t5qc33px/slot-131/k3xj-agbx_1080p/playlist.m3u8",
+    poster: "https://nirutv.online/logo/image.png"
   },
   ktv: {
     url: "https://webott.viatv.com.np/v0t1/KntCineplexTv.stream/playlist.m3u8",
