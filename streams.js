58db0b36cc0c62ef97f6aa62d86e2646ed58dcc6
wicketgoal <148673605+wicketgoal@users.noreply.github.com>
2025-11-01 10:13:19 +0545

    Update streams.js

--- streams.js
+++ streams.js
@@ -1,7 +1,7 @@
 // streams.js
 const streamMap = {
   nepal: {
-    url: "https://webott.viatv.com.np/v0t1/KntCineplexTv.stream/playlist.m3u8",
+    url: "https://webott.viatv.com.np/v0t1/KntC!neplexTv.stream/chunks.m3u8",
     poster: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiw5AUFwWq2uRT3Zoqjb9XCZHLKXN9C3XsmZYRZ1IPPP_EGSvchyoTf32GNS4XuBhOXJqTy8rUflS2A4VW-0i8f-aQkza4CJkF8ifHPSRJXNTp6nGsXSg-A36ZDWKpykSz2um5oAc8JGFOCZQ-aptX1wBGZCgjF5pkyQNOISu2HQ4_jw1G7YeqKLTDbMNo/s16000/Untitled%20design%20(6).png"
   },
   ktv: {
