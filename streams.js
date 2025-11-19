dcbfa34d23246633a486b80ba64a4938d776399c
wicketgoal <148673605+wicketgoal@users.noreply.github.com>
2025-11-19 16:20:59 +0545

    Update streams.js

--- streams.js
+++ streams.js
@@ -17,8 +17,8 @@ const streamMap = {
     poster: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiw5AUFwWq2uRT3Zoqjb9XCZHLKXN9C3XsmZYRZ1IPPP_EGSvchyoTf32GNS4XuBhOXJqTy8rUflS2A4VW-0i8f-aQkza4CJkF8ifHPSRJXNTp6nGsXSg-A36ZDWKpykSz2um5oAc8JGFOCZQ-aptX1wBGZCgjF5pkyQNOISu2HQ4_jw1G7YeqKLTDbMNo/s16000/Untitled%20design%20(6).png"
   },
 
-  fox1: {
-    url: "https://hugh.cdn.rumble.cloud/live/k5e12sb4/slot-101/v594-ykwe/chunklist.m3u8",
+  npl2: {
+    url: "https://hugh.cdn.rumble.cloud/live/u7gf26qm/slot-15/48hp-hg26/chunklist_DVR.m3u8",
     poster: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiw5AUFwWq2uRT3Zoqjb9XCZHLKXN9C3XsmZYRZ1IPPP_EGSvchyoTf32GNS4XuBhOXJqTy8rUflS2A4VW-0i8f-aQkza4CJkF8ifHPSRJXNTp6nGsXSg-A36ZDWKpykSz2um5oAc8JGFOCZQ-aptX1wBGZCgjF5pkyQNOISu2HQ4_jw1G7YeqKLTDbMNo/s16000/Untitled%20design%20(6).png"
   }
 };
