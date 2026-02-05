afa6367621e4138e4814cd3cec230a5b9e0cca27
wicketgoal <148673605+wicketgoal@users.noreply.github.com>
2026-02-05 19:08:00 +0545

    Update streams.js

--- streams.js
+++ streams.js
@@ -4,8 +4,8 @@ const streamMap = {
     as: "https://tvsen6.aynaott.com/asports/index.m3u8",
     poster: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiw5AUFwWq2uRT3Zoqjb9XCZHLKXN9C3XsmZYRZ1IPPP_EGSvchyoTf32GNS4XuBhOXJqTy8rUflS2A4VW-0i8f-aQkza4CJkF8ifHPSRJXNTp6nGsXSg-A36ZDWKpykSz2um5oAc8JGFOCZQ-aptX1wBGZCgjF5pkyQNOISu2HQ4_jw1G7YeqKLTDbMNo/s16000/Untitled%20design%20(6).png"
   },
-  bpl: {
-    url: "https://tvsen1.aynaott.com/YNMn5kZz8aLm/index.m3u8",
+  wpl: {
+    url: "https://mspannel.top/stream/rz/stream.php?id=18283d4f5f13c0cac5cab4433a0ed8f0&e=.m3u8",
     poster: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiw5AUFwWq2uRT3Zoqjb9XCZHLKXN9C3XsmZYRZ1IPPP_EGSvchyoTf32GNS4XuBhOXJqTy8rUflS2A4VW-0i8f-aQkza4CJkF8ifHPSRJXNTp6nGsXSg-A36ZDWKpykSz2um5oAc8JGFOCZQ-aptX1wBGZCgjF5pkyQNOISu2HQ4_jw1G7YeqKLTDbMNo/s16000/Untitled%20design%20(6).png"
   },
   bpl2: {
