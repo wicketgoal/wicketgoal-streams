ded326f0b609968dffaafeb9266257fb9c898aa9
wicketgoal <148673605+wicketgoal@users.noreply.github.com>
2026-02-07 15:08:59 +0545

    Update streams.js

--- streams.js
+++ streams.js
@@ -1,7 +1,7 @@
 // streams.js
 const streamMap = {
   ts: {
-    url: "https://live-en.aisports.cc/moviebox/device01/playlist.m3u8",
+    url: "https://live-en.aisports.cc/moviebox/device02/playlist.m3u8",
     poster: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiw5AUFwWq2uRT3Zoqjb9XCZHLKXN9C3XsmZYRZ1IPPP_EGSvchyoTf32GNS4XuBhOXJqTy8rUflS2A4VW-0i8f-aQkza4CJkF8ifHPSRJXNTp6nGsXSg-A36ZDWKpykSz2um5oAc8JGFOCZQ-aptX1wBGZCgjF5pkyQNOISu2HQ4_jw1G7YeqKLTDbMNo/s16000/Untitled%20design%20(6).png"
   },
   as: {
